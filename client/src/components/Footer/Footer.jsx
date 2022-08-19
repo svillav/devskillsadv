758338ef3a575aad6e8db9e46bc945ac66d9bbcf
Santiago Villa <santiagovillavega@gmail.com>
2022-08-19 08:23:41 -0300

    Footer component creation

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="flex flex-nowrap justify-between h-20 py-4 bg-black text-white">
+      <div className="mx-10">
+        <span>copyright</span>
+      </div>
+      <div className="mx-10">
+        <span>All rights reserved</span>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
