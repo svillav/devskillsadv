4be0b71c53a08f9d238d92be91c5f68c78ace462
Santiago Villa <santiagovillavega@gmail.com>
2022-08-19 08:28:37 -0300

    App layout modified to render components

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Form, Footer, Navbar } from './components';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Navbar />
       </header>
+      <main className='h-full bg-gray-900'>
+        <Form />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
